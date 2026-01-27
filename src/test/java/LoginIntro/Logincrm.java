23a85b9a3decd9facbafed3ef7431402e6457bda
huong-pham158 <myhuong158@gmail.com>
2026-01-27 12:22:10 +0700

    update file pom

--- src/test/java/LoginIntro/Logincrm.java
+++ src/test/java/LoginIntro/Logincrm.java
@@ -8,7 +8,7 @@ import java.time.Duration;
 
 public class Logincrm {
    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
--- src/test/java/ThucHanhL1/AddNewCustomer.java
+++ src/test/java/ThucHanhL1/AddNewCustomer.java
@@ -1,30 +1,4 @@
 package ThucHanhL1;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.time.Duration;
-
 public class AddNewCustomer {
-    public static void main(String[] args) throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        driver.get("https://crm.anhtester.com/admin/authentication");
-        driver.findElement(By.id("email")).sendKeys("admin@example.com");
-        driver.findElement((By.id("password"))).sendKeys("123456");
-        driver.findElement(By.xpath("//button[normalize-space()='Login']")).click();
-        Thread.sleep(2000);
-
-        driver.findElement(By.xpath("//span[normalize-space()='Customers']")).click();
-        Thread.sleep(1000);
-
-        System.out.println(driver.findElement(By.xpath("//span[normalize-space()='Active Customers']")).getCssValue("color"));
-
-        driver.quit();
-
-
-    }
 }
